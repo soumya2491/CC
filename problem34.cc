f791cc7bb7fb01f1f0ad85321b1bf0e6c9d4a7d6
soumya2491 <soumyachopra901@gmail.com>
2025-09-04 22:11:04 +0530

    practicing more problems through recursion

---
+++ problem34.cc
@@ -0,0 +1,29 @@
+#include <iostream>
+
+void draw(int s)
+{
+    if (s == 1)
+        return;
+    else
+    {
+        // for (int i = 0; i < s - 2; i++)
+        // {
+        //     std::cout << "  ";
+        // }
+        for (int i = 0; i < s - 1; i++)
+        {
+            std::cout << "* ";
+        }
+        std::cout << std::endl;
+    }
+    draw(s - 1);
+}
+
+
+int main(void)
+{
+    int s;
+    std::cin >> s;
+    draw(s);
+    return 0;
+}
